1af2fa600dbf8258547b75d0746a46bd69166599
MDSULTRA69 <abedarasimi20@gmail.com>
2026-05-23 10:44:51 +0100

    Update battles.js

--- src/routes/battles.js
+++ src/routes/battles.js
@@ -159,14 +159,14 @@ router.post('/:id/action', auth, async (req, res) => {
 
       if (battle.player1HP <= 0 && battle.player2HP <= 0) {
         battle.isDraw = true;
-        battle.endReason = 'Both players KO'd';
+        battle.endReason = "Both players KO'd";
         battle.winner = battle.player2._id;
         battle.loser = battle.player1._id;
-        battle.endReason = 'Player 1 KO'd';
+        battle.endReason = "Player 1 KO'd";
       } else if (battle.player2HP <= 0) {
         battle.winner = battle.player1._id;
         battle.loser = battle.player2._id;
-        battle.endReason = 'Player 2 KO'd';
+        battle.endReason = "Player 2 KO'd";
       } else {
         // Turn limit reached - compare damage
         const p1Damage = 100 - battle.player2HP;
